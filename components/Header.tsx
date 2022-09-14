bc59075ee1ac928b8ee1f4b9dc2c93093a34011e
Matt Gehrls <mattgehrls@gmail.com>
2022-09-14 18:51:42 -0400

    search gets saved to state

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './Header.module.css'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios'
+const apiKey = "api_key=4cc551bdbec360295f6123a443e43bb9"
 
-export default function Header() {
+interface HeaderProps{
+    search: (type:string, searchItem:string)=> Promise<false | undefined>
+}
+
+export default function Header({search}: HeaderProps) {
   return (
+    <>
     <header className={styles.header}>
         <a className={styles.titleContainer} href='/'>
             <div>
-                <h1 className={styles.pageTitle}>Find your film</h1>
+                <h1 className={styles.pageTitle}>Streaming Watchlist</h1>
                 <p className={styles.pageSubtitle}>data and images courtesy of tmdb.org</p>
             </div>
         </a>
@@ -15,5 +24,23 @@ export default function Header() {
             <a href="/tv">TV</a>
         </nav>
     </header>
+        <form 
+            onSubmit={(e)=>{
+                e.preventDefault()
+                //setSearching(true)
+                search("multi", (document.getElementById("searchinput") as HTMLInputElement).value)
+            }} 
+            className={styles.searchbar}>
+            <input 
+                id={"searchinput"}
+                className={styles.searchinput}
+                type="text" 
+                placeholder={"test placeholder"}/>
+            <FontAwesomeIcon 
+                icon={faMagnifyingGlass} 
+                className={styles.searchicon} 
+                onClick={() => search("multi", (document.getElementById("searchinput") as HTMLInputElement).value)}/>
+        </form>
+        </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,128 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
-import Searchbar from '../components/Searchbar'
 import axios from 'axios'
+import { useState, useEffect } from 'react'
 const apiKey = "api_key=4cc551bdbec360295f6123a443e43bb9"
 
 const Home: NextPage = () => {
-  
+  const [data, setData] = useState<SearchData | null>(null)
+  interface SearchResult{
+    adult?: boolean;
+    backdrop_path?: string;
+    genre_ids?:number[];
+    id: number;
+    media_type?: string;
+    original_language?:string;
+    original_title?:string;
+    overview?:string;
+    popularity?: number;
+    poster_path?:string;
+    realease_date?:string;
+    title?:string;
+    video?:boolean;
+    vote_average?:number;
+    vote_count?:number;
+  }
+  interface SearchData{
+    data:{
+      page: number;
+      results: SearchResult[];
+      total_pages: number;
+      total_results: number;
+    }
+  }
 
+  useEffect(()=>{
+    console.log(data)
+  },[data])
+
+  async function runSearch(type: string, searchItem: string | null){
+    let url = ""
+    
+    switch(type){
+      case ("multi"):
+        url = `https://api.themoviedb.org/3/search/multi?${apiKey}&language=en-US&query=${searchItem}&page=1&include_adult=false`
+        
+        if(searchItem){
+          searchItem = encodeURI(searchItem)
+          await axios.get(url)
+              .then((response) => {
+                  setData(response.data.results)
+                  console.log(response)
+                  return true
+            })
+            .catch((err) => console.log(err))
+            .finally(()=>{
+              console.log("search complete")
+            })
+        }else{
+          return false
+        }
+            break
+      case("movie"):
+        url = `https://api.themoviedb.org/3/search/movie?${apiKey}&language=en-US&query=${searchItem}&page=1&include_adult=false`
+        
+        if(searchItem){
+          searchItem = encodeURI(searchItem)
+          await axios.get(url)
+              .then((response) => {
+                  console.log(response)
+                  return true
+
+            })
+            .catch((err) => console.log(err))
+            .finally(()=>{
+              console.log("search complete")
+            })
+        }else{
+          return false
+        }
+
+        break
+      case("tv"):
+        url = `https://api.themoviedb.org/3/search/tv?${apiKey}&language=en-US&page=1&query=${searchItem}&include_adult=false`
+        
+        if(searchItem){
+          searchItem = encodeURI(searchItem)
+          await axios.get(url)
+            .then((response) => {
+              console.log(response)
+              return true
+            })
+            .catch((err) => console.log(err))
+            .finally(()=>{
+              console.log("search complete")
+            })
+        }else{
+          return false
+        }
+
+        break
+      default:
+        url = `https://api.themoviedb.org/3/search/multi?${apiKey}&language=en-US&query=${searchItem}&page=1&include_adult=false`
+        
+        if(searchItem){
+          searchItem = encodeURI(searchItem)
+          await axios.get(url)
+              .then((response) => {
+                  console.log(response)
+                  return true
+            })
+            .catch((err) => console.log(err))
+            .finally(()=>{
+              console.log("search complete")
+            })
+        }else{
+          return false
+        }
+        break
+    }
+
+  }
+  const HeaderProps = {
+    search: runSearch
+  }
 
   return (
     <div className={styles.container}>
@@ -17,9 +132,9 @@ const Home: NextPage = () => {
         <meta name="description" content="Keep track of your favorite shows and movies!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header {...HeaderProps}/>
       <div className={styles.main}>
-        <Searchbar/>
+
       </div>
     </div>
   )
